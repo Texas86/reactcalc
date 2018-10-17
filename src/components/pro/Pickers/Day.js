84543196ba3ff3fe35d4c7c0babfe14c68b9c8d2
Jakub Mandra <j.mandra@mdbootstrap.com>
2018-10-17 11:34:05 +0200

    Separate imports, add Days disabeling function

--- src/components/pro/Pickers/Day.js
+++ src/components/pro/Pickers/Day.js
@@ -1,11 +1,19 @@
 import React from "react";
 import format from "date-fns/format";
+import isWithinRange from "date-fns/is_within_range";
+import startOfMonth from "date-fns/start_of_month";
+import endOfMonth from "date-fns/end_of_month";
 import Button from "../../Button";
 
-const Day = ({ day }) => {
+const Day = ({ day, date }) => {
   return (
     <div>
-      <Button floating>{format(day, "D")}</Button>
+      <Button
+        disabled={!isWithinRange(day, startOfMonth(date), endOfMonth(date))}
+        floating
+      >
+        {format(day, "D")}
+      </Button>
     </div>
   );
 };
--- src/components/pro/Pickers/Month.js
+++ src/components/pro/Pickers/Month.js
@@ -1,20 +1,25 @@
 import React from "react";
-import * as DateFns from "date-fns";
+import startOfWeek from "date-fns/start_of_week";
+import startOfMonth from "date-fns/start_of_month";
+import endOfWeek from "date-fns/end_of_week";
+import endOfMonth from "date-fns/end_of_month";
+import isBefore from "date-fns/is_before";
+import addDays from "date-fns/add_days";
 import Day from "./Day";
 
 const getWeeksArray = date => {
-  const start = DateFns.startOfWeek(DateFns.startOfMonth(date));
-  const end = DateFns.endOfWeek(DateFns.endOfMonth(date));
+  const start = startOfWeek(startOfMonth(date));
+  const end = endOfWeek(endOfMonth(date));
 
   let count = 0;
   let current = start;
   const nestedWeeks = [];
 
-  while (DateFns.isBefore(current, end)) {
+  while (isBefore(current, end)) {
     const weekNumber = Math.floor(count / 7);
     nestedWeeks[weekNumber] = nestedWeeks[weekNumber] || [];
     nestedWeeks[weekNumber].push(current);
-    current = DateFns.addDays(current, 1);
+    current = addDays(current, 1);
     count += 1;
   }
 
@@ -27,7 +32,7 @@ const Month = ({ date }) => {
       {getWeeksArray(date).map((week, index) => (
         <div key={`week-${week[index].toString()}`} className="d-flex">
           {week.map(day => (
-            <Day key={day.toString()} day={day} />
+            <Day key={day.toString()} day={day} date={date} />
           ))}
         </div>
       ))}
